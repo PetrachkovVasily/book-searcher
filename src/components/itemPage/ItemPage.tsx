d6da4718cf547b3eca42e60b626df795c66ea841
PetrachkovVasiliy <vasyapt04mail.com>
2024-06-19 10:08:32 +0300

    Task 10

--- src/components/itemPage/ItemPage.tsx
+++ src/components/itemPage/ItemPage.tsx
@@ -9,7 +9,14 @@ export function ItemPage() {
           <Book/>
         </div>
         <div className={classes.descriptionContainer}>
-
+          <h3 className={classes.decoratedHeader}>Art/General</h3>
+          <div className={classes.nameDescription}>
+            <h2 className={classes.bookName}>Book name</h2>
+            <h3 className={classes.decoratedHeader}>Author Name</h3>
+          </div>
+          <p className={classes.details}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </p>
         </div>
       </div>
     </main>
